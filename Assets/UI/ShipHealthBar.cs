38f3591643e2703af40066182faa6085e5220a91
Hoàng Hà <hah_57@vnu.edu.vn>
2014-12-13 23:44:14 +0700

    finished first UI tutorial

---
+++ Assets/UI/ShipHealthBar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShipHealthBar : MonoBehaviour {
+
+	public Scrollbar HealthBar;
+	public float Health = 100;
+
+	public void damage(float value){
+		Health -= value;
+		HealthBar.size = Health / 100f;
+	}
+}
