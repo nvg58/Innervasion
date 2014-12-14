e494ad578ee1dd539c7e73baff267b7b430d0891
nvg58 <nvangiap5894@gmail.com>
2014-12-15 00:04:48 +0700

    simulate 3 featured ee

---
+++ Assets/Scripts/MH.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+ 
+var acceleration : float;
+var maxMagnitudeMHCanSuffer : float;
+private var myGravity: ConstantForce;
+
+
+function Start(){
+	rigidbody.useGravity = false; // forget about physics default gravity
+	myGravity = gameObject.AddComponent(ConstantForce); // add a ConstantForce component
+	SetGravity(9.81 * Vector3.down); // set regular gravity
+}
+ 
+function FixedUpdate ()
+{
+    var xIndex : float = Input.GetAxis("Horizontal") * acceleration;
+    var yIndex : float = Input.GetAxis("Vertical") * acceleration;
+//    SetGravity(2.5 * Vector3.left); 
+   	transform.Translate(xIndex, yIndex, 0);
+}
+
+
+function OnCollisionEnter2D( col : Collision2D )
+{
+	Debug.Log ( col.relativeVelocity.magnitude );
+	if(col.relativeVelocity.magnitude > maxMagnitudeMHCanSuffer)
+	{ //remove this 
+		Debug.Log ("Damn it!");
+	}  
+ }  
+ 
+function SetGravity(g: Vector3){
+	// calculate the necessary force to produce the desired gravity:
+	myGravity.force = rigidbody.mass * g; 
+}
+
---
+++ Assets/Scripts/MHControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MHControl : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 5.0f;
+//		rigidbody2D.velocity = Vector2.one.normalized * speed;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector2 curVel = gameObject.rigidbody2D.velocity;
+		curVel.x = Input.GetAxis("Horizontal") * speed ; // max speed = 5
+		curVel.y = Input.GetAxis("Vertical") * speed; // max speed = 5
+		gameObject.rigidbody2D.velocity = curVel;
+
+
+	}
+	
+ 	void OnCollisionEnter2D (Collision2D other) {
+		rigidbody2D.velocity = Vector2.one.normalized * speed;
+		Debug.Log ("hello");
+	}
+}
