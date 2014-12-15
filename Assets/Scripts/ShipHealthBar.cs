392f5825b4c8aff9077a422abfddf6cad832e8eb
Hoàng Hà <hah_57@vnu.edu.vn>
2014-12-15 20:00:42 +0700

    update UI

---
+++ Assets/Scripts/ShipHealthBar.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShipHealthBar : MonoBehaviour {
+
+	public Scrollbar HealthBar;
+	public float Health = 100;
+	public Slider Stamina;
+
+	public void damage(float value){
+		Health -= value;
+		HealthBar.size = Health / 100f;
+
+		Stamina.value = Health / 100f;
+	}	
+}
