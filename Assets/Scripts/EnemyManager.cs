2afb0fc925236a9280d99e809f1618ee3707b73d
Giap Nguyen <nvangiap5894@gmail.com>
2015-02-10 17:19:42 +0700

    prevent spawn enemy inside MH

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -7,12 +7,14 @@ public class EnemyManager : MonoBehaviour
 		public GameObject enemy;                // The enemy prefab to be spawned.
 		public float spawnTime = 3f;            // How long between each spawn.
 		public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
-	
+		
+		GameObject MH;
 	
 		void Start ()
 		{
 				// Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
 				InvokeRepeating ("Spawn", spawnTime, spawnTime);
+				MH = GameObject.FindGameObjectWithTag ("Player");
 		}
 	
 		void Spawn ()
@@ -26,8 +28,9 @@ public class EnemyManager : MonoBehaviour
 		
 				// Find a random index between zero and one less than the number of spawn points.
 				int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-		
-				// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-				Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+				if (!MH.GetComponent<PolygonCollider2D> ().bounds.Contains (spawnPoints [spawnPointIndex].position)) {
+						// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
+						Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+				}
 		}
 }
