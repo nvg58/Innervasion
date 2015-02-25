c537e852c50f65d9905b2666569980aabb09af61
Giap Nguyen <nvangiap5894@gmail.com>
2015-02-25 17:44:27 +0700

    spawn  enemy effects

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -6,9 +6,12 @@ public class EnemyManager : MonoBehaviour
 //	public PlayerHealth playerHealth;       // Reference to the player's heatlh.
 		public GameObject enemy;                // The enemy prefab to be spawned.
 		public float spawnTime = 3f;            // How long between each spawn.
+		public float timeToActualSpawnEnemy = 2f;	// How long between spawn effect and enemy
 		public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
-		
+		public GameObject energyBlast;
 		GameObject MH;
+		
+		private int spawnPointIndex;
 	
 		void Start ()
 		{
@@ -27,10 +30,18 @@ public class EnemyManager : MonoBehaviour
 //		}
 		
 				// Find a random index between zero and one less than the number of spawn points.
-				int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-//				if (!MH.GetComponent<PolygonCollider2D> ().bounds.Contains (spawnPoints [spawnPointIndex].position)) {
-						// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-						Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+				spawnPointIndex = Random.Range (0, spawnPoints.Length);
+//				if (!MH.GetComponent<PolygonCollider2D> ().bounds.Contains (spawnPoints [spawnPointIndex].position)) {						
+				// Spawn enemy appear effect first
+				Instantiate (energyBlast, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+				// Then wait timeToActualSpawnEnemy seconds to actual spawn enemy
+				Invoke ("ActualSpawnEnemy", timeToActualSpawnEnemy);						
 //				}
+		}
+
+		void ActualSpawnEnemy ()
+		{
+				// Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.											
+				Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
 		}
 }
