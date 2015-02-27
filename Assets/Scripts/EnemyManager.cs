ac6ebb9fdca9510f31594c3f08a88018fc0ee852
Giap Nguyen <nvangiap5894@gmail.com>
2015-02-27 18:33:51 +0700

    finish runable version

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class EnemyManager : MonoBehaviour
 {
-//	public PlayerHealth playerHealth;       		// Reference to the player's heatlh.
 		public GameObject enemy;                	// The enemy prefab to be spawned.
 		public float spawnTime = 3f;            	// How long between each spawn.
 		public float timeToActualSpawnEnemy = 2f;	// How long between spawn effect and enemy
@@ -22,21 +21,12 @@ public class EnemyManager : MonoBehaviour
 	
 		void Spawn ()
 		{
-//		// If the player has no health left...
-//		if(playerHealth.currentHealth <= 0f)
-//		{
-//			// ... exit the function.
-//			return;
-//		}
-		
 				// Find a random index between zero and one less than the number of spawn points.
-				spawnPointIndex = Random.Range (0, spawnPoints.Length);
-//				if (!MH.GetComponent<PolygonCollider2D> ().bounds.Contains (spawnPoints [spawnPointIndex].position)) {						
+				spawnPointIndex = Random.Range (0, spawnPoints.Length); 
 				// Spawn enemy appear effect first
 				energyBlast = Instantiate (energyBlastPrefab, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation) as GameObject;
 				// Then wait timeToActualSpawnEnemy seconds to actual spawn enemy
 				Invoke ("ActualSpawnEnemy", timeToActualSpawnEnemy);						
-//				}
 		}
 
 		void ActualSpawnEnemy ()
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,8 +6,12 @@ public class EnemyMovement : MonoBehaviour
 		Transform MH;
 		public float MoveSpeed = 3.0f;
 		public float MaxDist = 10.0f;
-		public float MinDist = 5.0f;
+		public float MinDist = 6.0f;
+		public float SafeDist = 4.0f;
 		public GameObject bulletPrefab;
+		private float tChange = 0f; // force new direction in the first Update 
+		private float randomX;
+		private float randomY;
 
 		void Start ()
 		{
@@ -24,18 +28,41 @@ public class EnemyMovement : MonoBehaviour
 		}
 	
 		void Update ()
-		{
+		{		
+				Debug.Log ("dist: " + Vector3.Distance (this.transform.position, MH.position));
 				if (Vector3.Distance (transform.position, MH.position) >= MinDist) {
-						transform.position += (MH.transform.position - transform.position).normalized * MoveSpeed * Time.deltaTime;							
-				} 				
-		}
+						transform.position += (MH.transform.position - transform.position).normalized * MoveSpeed * Time.deltaTime;
 
+						
+				} else {
+						if (Time.time >= tChange) {
+								randomX = Random.Range (-0.5f, 0.5f); // with float parameters, a random float
+								randomY = Random.Range (-0.5f, 0.5f); //  between -2.0 and 2.0 is returned
+								// set a random interval between 0.5 and 1.5
+								tChange = Time.time + Random.Range (0.5f, 1.5f);
+						}
+						Vector3 randPos = new Vector3 (randomX, randomY, 0);				
+						Vector3 futurePos = transform.position + randPos * MoveSpeed * Time.deltaTime;
+			
+						Debug.Log ("pos: " + transform.position + " randPos: " + randPos + " futurePos: " + futurePos + " dis: " + Vector3.Distance (futurePos, MH.position));
+			
+						if (Vector3.Distance (futurePos, MH.position) > SafeDist) {
+								transform.position = futurePos;					
+								Vector3 delta = MH.transform.position - transform.position;
+								float angle = - Mathf.Atan2 (delta.x, delta.y) * Mathf.Rad2Deg;
+								Quaternion rot = Quaternion.Euler (new Vector3 (0, 0, angle));
+								transform.localRotation = Quaternion.Lerp (transform.localRotation, rot, Time.deltaTime / 3);
+						} else {
+				
+						}
+				}    	    
+		}
+	
 		void Shoot ()
 		{
 				if (Vector3.Distance (this.transform.position, MH.position) <= MaxDist) {
 						GameObject bullet = Instantiate (bulletPrefab, GetChildByName ("firePoint").position, transform.rotation) as GameObject;									
 				} 
-				Debug.Log ("Shoot at " + Time.time);
 		}
 
 		Transform GetChildByName (string name)
