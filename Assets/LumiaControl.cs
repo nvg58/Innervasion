3401ab58e0a1d352487a9adfb1ea64ecd477792c
Giap Nguyen <nvangiap5894@gmail.com>
2015-03-17 18:21:36 +0700

    add Walk tutorial

---
+++ Assets/LumiaControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LumiaControl : MonoBehaviour
+{
+
+		Vector3 initPos;
+		GameObject Milo;
+
+		// Use this for initialization
+		void Start ()
+		{
+				Milo = GameObject.Find ("Milo");
+				initPos = Milo.transform.position;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (Vector3.Distance (initPos, Milo.transform.position) > 0.8f) {
+						Animator animator = gameObject.GetComponent<Animator> ();
+						animator.SetTrigger ("Awesome");
+				}
+		}
+}
