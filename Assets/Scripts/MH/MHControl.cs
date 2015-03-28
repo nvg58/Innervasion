975ff81451f0cf50708020848e26461c554879cb
Giap Nguyen <nvangiap5894@gmail.com>
2015-03-28 19:49:45 +0700

    add puff 4 directions

--- Assets/Scripts/MH/MHControl.cs
+++ Assets/Scripts/MH/MHControl.cs
@@ -6,6 +6,8 @@ public class MHControl : MonoBehaviour {
 	public float moveForce = 70.0f;
 	public Vector2 velocity = Vector2.zero;
 	public float frictionCoefficient = 0.4f;
+
+	public GameObject puffPrefab;
 	
 	public void FixedUpdate (){
 		// apply Archimedes force
@@ -35,6 +37,26 @@ public class MHControl : MonoBehaviour {
 			rigidbody2D.AddForce(Vector2.right * HInput * moveForce);
 			rigidbody2D.AddForce(Vector2.up * VInput * moveForce);
 		}
+
+		if (HInput > 0) {
+			Instantiate(puffPrefab, GetChildByName("pufflt").position, GetChildByName("pufflb").rotation);
+			Instantiate(puffPrefab, GetChildByName("pufflb").position, GetChildByName("pufflb").rotation);
+		}
+
+		if (HInput < 0) {
+			Instantiate(puffPrefab, GetChildByName("puffrt").position, GetChildByName("pufflb").rotation);
+			Instantiate(puffPrefab, GetChildByName("puffrb").position, GetChildByName("pufflb").rotation);
+		}
+
+		if (VInput < 0) {
+			Instantiate(puffPrefab, GetChildByName("pufftl").position, Quaternion.Euler(0, 0, 90));
+			Instantiate(puffPrefab, GetChildByName("pufftr").position, Quaternion.Euler(0, 0, 90));
+		}
+
+		if (VInput > 0) {
+			Instantiate(puffPrefab, GetChildByName("puffbl").position, Quaternion.Euler(0, 0, 90));
+			Instantiate(puffPrefab, GetChildByName("puffbr").position, Quaternion.Euler(0, 0, 90));
+		}
 			
 		Vector2 vel = rigidbody2D.velocity;
 		float tmp = Mathf.Sqrt((vel.x*vel.x + vel.y*vel.y) / (maxSpeed*maxSpeed));
@@ -44,6 +66,15 @@ public class MHControl : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D( Collision2D col ) {
+	}
+
+	Transform GetChildByName (string name)
+	{
+		foreach (Transform t in transform) {
+			if (t.name == name)
+				return t;
+		}
+		return null;
 	}
 }
 
